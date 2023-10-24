ed6e2a5cb1707fa8facb1b986b0a0be35ee415d8
vmalaviya <varunmalaviya7@gmail.com>
2023-10-23 23:32:18 -0400

    creating a skeleton setup to test page transitions

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link"
+
 function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className="w-full flex items-center justify-between">
+            <Link href={'/'}>
+                Home
+            </Link>
+
+            <div className="flex items-center justify-center gap-10">
+                <Link href={'/about'}>
+                    About
+                </Link>
+                <Link href={'/portfolio'}>
+                    Portfolio
+                </Link>
+                <Link href={'/contact'}>
+                    Contact
+                </Link>
+            </div>
         </div>
     )
 }

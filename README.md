e692365a20fb4bd5b601619f686830b52a2d760f
vmalaviya <varunmalaviya7@gmail.com>
2023-10-25 12:59:16 -0400

    added a masonry grid in portfolio section with ability to filter images based on categories

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 <h1>Photographer Portfolio Website</h1>
+
+Image Credits:
+
+- Photo by <a href="https://unsplash.com/@neganova?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Valeriia Neganova</a> on <a href="https://unsplash.com/photos/an-aerial-view-of-houses-in-the-woods-TdEs32gq79A?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- Photo by <a href="https://unsplash.com/@von_co?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ivana Cajina</a> on <a href="https://unsplash.com/photos/milky-way-asuyh-_ZX54?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- Photo by <a href="https://unsplash.com/@aldebarans?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Aldebaran S</a> on <a href="https://unsplash.com/photos/blue-and-black-galaxy-digital-wallpaper-qtRF_RxCAo0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- Photo by <a href="https://unsplash.com/@aldebarans?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Aldebaran S</a> on <a href="https://unsplash.com/photos/purple-and-black-galaxy-illustration-uXchDIKs4qI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- Photo by <a href="https://unsplash.com/@nasa?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">NASA</a> on <a href="https://unsplash.com/photos/view-of-earth-and-satellite-yZygONrUBe8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,94 @@
+'use client'
+import { Tabs, Tab,  } from "@mui/material"
+import { useState } from "react"
+import Masonry from '@mui/lab/Masonry'
+import Image from "next/image"
+import useMediaQuery from "@/hooks/useMediaQuery"
+
+function Page() {
+    const [value, setValue] = useState<string>('all')
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue)
+    }
+
+    const images = [
+        {
+            image: '/Earth1.jpg',
+            tag: 'Earth'
+        },
+        {
+            image: '/Space1.jpg',
+            tag: 'Space'
+        },
+        {
+            image: '/Space2.jpg',
+            tag: 'Space'
+        },
+        {
+            image: '/Space3.jpg',
+            tag: 'Space'
+        },
+        {
+            image: '/Space4.jpg',
+            tag: 'Space'
+        }
+    ]
+
+    //determine screensize
+    const aboveSmallScreens = useMediaQuery('(min-width: 768px)')
+
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between bg-black">
+            <div className="container pt-20 text-white flex flex-col items-center">
+                <h1 className="text-5xl text-center">PORTFOLIO</h1>
+
+                {/* Tabs for filtering categories */}
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    textColor="inherit"
+                    indicatorColor="primary"
+                    aria-label="secondary tabs example"
+                    className="my-5"
+                >
+                    <Tab value="all" label="All" />
+                    <Tab value="Space" label="Space" />
+                    <Tab value="Earth" label="Earth" />
+                </Tabs>
+
+                {/* Masonry grid for images */}
+                <Masonry columns={ aboveSmallScreens ? 3 : 1 } spacing={2}>
+                    {value === 'all' 
+                        ?
+                        (
+                            images.map((pic, index) => (
+                                <Image 
+                                    key={index} 
+                                    src={pic.image}
+                                    alt="image"
+                                    height={200}
+                                    width={250}   
+                                />
+                            ))
+                        )
+                        :
+                        (
+                            images.filter(pic => pic.tag === value).map((pic,index) => 
+                                <Image 
+                                    key={index} 
+                                    src={pic.image}
+                                    alt="image"
+                                    height={200}
+                                    width={250}   
+                                />
+                            )
+                        )
+                    }
+                </Masonry>
+            </div>
+        </div>
+    )
+}
+
+export default Page

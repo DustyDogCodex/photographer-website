da8db780d8e1a0ea7052d239e26eba69f1a99c0c
vmalaviya <varunmalaviya7@gmail.com>
2023-11-01 13:55:17 -0400

    implemented parallax effect in hero component. Image needs to be retouched since of the layers were not vut as cleanly as I expected

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,11 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend:{ 
+        colors: {
+            'black-smoke': 'linear-gradient(180deg, #ffffff 0%, #FFF 100%)'
+        },
+    }
   },
   plugins: [],
 }

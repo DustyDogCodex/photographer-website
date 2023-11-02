3a52bdd62ca2b6bf88081ad251ad41c3e3aa57a3
vmalaviya <varunmalaviya7@gmail.com>
2023-11-02 13:36:56 -0400

    refactored code for better readability and organisation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Hero from "@/components/Hero";
+import Hero from "@/components/Hero"
+import About from "../components/About"
+import CardGrid from "../components/CardGrid"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between bg-black">
-        <Hero />
+    <main className="flex w-full flex-col items-center justify-between bg-black">
+        <div className="parallax-wrapper text-white w-full">
+            <Hero />
+            <CardGrid />
+            <About />
+            <Footer />
+        </div>
     </main>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,34 @@
 import Image from "next/image"
-import About from "./About"
-import CardGrid from "./CardGrid"
-import Footer from "./Footer"
 
+{/* parallax images and text */}
 function Hero() {
     return (
-        <div className="parallax-wrapper text-white w-full">
-            {/* parallax images and text */}
-            <header>
-                {/* background image */}
-                <Image 
-                    src='/mountain3.png'
-                    alt=""
-                    width={300}
-                    height={300}
-                    className="background w-full h-full absolute top-0 object-cover -z-10"
-                    unoptimized={true}
-                />
+        <header>
+            {/* background image */}
+            <Image 
+                src='/mountain3.png'
+                alt=""
+                width={300}
+                height={300}
+                className="background w-full h-full absolute top-0 object-cover -z-10"
+                unoptimized={true}
+            />
 
-                {/* foreground image */}
-                <Image 
-                    src='/couple.png'
-                    alt=""
-                    width={300}
-                    height={300}
-                    className="foreground w-full h-full absolute top-0 object-cover -z-10"
-                    unoptimized={true}
-                />
+            {/* foreground image */}
+            <Image 
+                src='/couple.png'
+                alt=""
+                width={300}
+                height={300}
+                className="foreground w-full h-full absolute top-0 object-cover -z-10"
+                unoptimized={true}
+            />
 
-                <h1 className="text-5xl md:text-8xl font-marker">Awe Inspiring</h1>
+            <h1 className="text-5xl md:text-8xl font-marker">Awe Inspiring</h1>
 
-                {/* black smokey effect at the bottom */}
-                <div className="absolute h-[150px] bottom-0 w-full bg-gradient-to-t from-black via-black/90 to-transparent" />
-            </header>
-            
-            <CardGrid />
-            <About />
-            <Footer />
-        </div>
+            {/* black smokey effect at the bottom */}
+            <div className="absolute h-[150px] bottom-0 w-full bg-gradient-to-t from-black via-black/90 to-transparent" />
+        </header>
     )
 }
 
